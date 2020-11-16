009f83751b913f944aa0093a5b32b985fc285e15
jieun <bje0825@naver.com>
2020-11-16 09:55:03 +0900

    adding logic and conditionals

---
+++ game.py
@@ -0,0 +1,36 @@
+# import packages to ectend python (just like we ectend sublime, or Atom, or VSCode)
+from random import randint
+
+# [ ] => this is an array, an array is a special type of container that can hold multipe items.
+# arrays are indexed (their contents get assigned a number)
+# the index always starts at 0 
+choices = ["rock", "paper", "scissors"]
+
+# this is the player choice
+player = input("choose rock, paper or scissors : ")
+
+# this will be the AI choice -> a random pick from the choices array
+computer = choices[randint(0,2)]
+
+# check to see what the user input
+
+# print outputs whatever is in the round brackets => in this case it ouput player to the command prompt window
+print("user chose: " + player)
+
+# validate that the random choice worked for the AI
+print("AI chose: " + computer)
+
+if (computer == player):
+	print("tie")
+
+elif(computer== "rock"):
+	if (player == "scissors"):
+		print("you lose")
+	else:
+		print("you win")
+
+elif(computer== "paper"):
+	if (player == "scissors"):
+		print("you win")
+	else:
+		print("you lose")
